50c9f80a5fda1b4e4f075a0a31c8b514b6f60275
Lukasz Awerjanow <awerxx@gmail.com>
2024-05-08 06:59:50 +0200

    Empty admin API template

---
+++ src/Zoya.Api/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Avvr.Kappusta.Zoya.Api.Controllers;
+
+public class AdminController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
